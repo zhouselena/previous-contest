c00ec0a7974ee5b1469ddd65e8ebee748c9485e8
Selena <Selena@ACER_SELENA>
2020-06-26 17:50:46 -0400

    finished optimized version

--- CCC/src/SR2015/SR32015.java
+++ CCC/src/SR2015/SR32015.java
@@ -1,41 +1,50 @@
 package SR2015;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SR32015 {
 
 	public int numbGates;
 	public int numbPlanes;
-	public int[] gates;
+	public Map<Integer, Integer> nextGate = null;
 	
 	public SR32015(int g, int p) {
 		this.numbGates = g;
 		this.numbPlanes = p;
-		this.gates = new int[numbGates];
+		this.nextGate = new HashMap<Integer, Integer>();
 	}
 	
-	public int nextInd(int maxGate) {
-		int gate = -1;
-		for (int i = maxGate; i >= 0; i--) {
-			if (gates[i]==0) {
-				gate = i;
-				break;
-			}
+	public boolean dockPlane(int maxGate) {
+		int next = maxGate;
+		ArrayList<Integer> update = new ArrayList<Integer>();
+		while (nextGate.containsKey(next) && next > 0) {
+			update.add(next);
+			next = nextGate.get(next);
 		}
-		return gate;
+		nextGate.put(next, next-1);
+		for (int i: update) {
+			nextGate.put(i, next-1);
+		}
+		if (next > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public int run(int[] planes) {
 		int numbDocked = 0;
 		
-		for (int i = 0; i < numbPlanes; i++) {
-			int ind = nextInd(planes[i]);
-			if (ind==-1) {
-				break;
+		for (int i: planes) {
+			if (dockPlane(i)) {
+				numbDocked++;
 			}
 			else {
-				gates[ind] = 1;
-				numbDocked++;
+				break;
 			}
 		}
 		
@@ -49,7 +58,7 @@ public class SR32015 {
 		SR32015 game = new SR32015(Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine()));
 		int[] planes = new int[game.numbPlanes];
 		for (int i = 0; i < planes.length; i++) {
-			planes[i] = Integer.parseInt(sc.nextLine())-1;
+			planes[i] = Integer.parseInt(sc.nextLine());
 		}
 		
 		System.out.println(game.run(planes));
---
+++ CCC/src/SR2015/SR32015V2.java
@@ -0,0 +1,60 @@
+package SR2015;
+
+import java.util.Scanner;
+
+public class SR32015V2 {
+
+	public int numbGates;
+	public int numbPlanes;
+	public int[] gates;
+	
+	public SR32015V2(int g, int p) {
+		this.numbGates = g;
+		this.numbPlanes = p;
+		this.gates = new int[numbGates];
+	}
+	
+	public int nextInd(int maxGate) {
+		int gate = -1;
+		for (int i = maxGate; i >= 0; i--) {
+			if (gates[i]==0) {
+				gate = i;
+				break;
+			}
+		}
+		return gate;
+	}
+	
+	public int run(int[] planes) {
+		int numbDocked = 0;
+		
+		for (int i = 0; i < numbPlanes; i++) {
+			int ind = nextInd(planes[i]);
+			if (ind==-1) {
+				break;
+			}
+			else {
+				gates[ind] = 1;
+				numbDocked++;
+			}
+		}
+		
+		return numbDocked;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		SR32015V2 game = new SR32015V2(Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine()));
+		int[] planes = new int[game.numbPlanes];
+		for (int i = 0; i < planes.length; i++) {
+			planes[i] = Integer.parseInt(sc.nextLine())-1;
+		}
+		
+		System.out.println(game.run(planes));
+		
+		sc.close();
+	}
+
+}
