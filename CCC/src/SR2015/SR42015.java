63259d287a93ee5e8f2462d14d97e60b10cbb9fd
Selena <Selena@ACER_SELENA>
2020-06-27 11:11:14 -0400

    started framework of s4

---
+++ CCC/src/SR2015/SR42015.java
@@ -0,0 +1,29 @@
+package SR2015;
+
+public class SR42015 {
+	
+	public class SeaRoute {
+		public int to;
+		public int from;
+		public int time;
+		public int wear;
+		
+		public SeaRoute(int t, int f, int ti, int w) {
+			this.to = t;
+			this.from = f;
+			this.time = ti;
+			this.wear = w;
+		}
+	}
+	
+	public int hullOg = 0;
+	public int numbIsland = 0;
+	public int numbRoute = 0;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
