50434ea8e6dfcf82f3deb922c8c5e607b6eda7bb
Selena <Selena@ACER_SELENA>
2020-08-03 22:16:41 -0400

    have an idea but still thinking

--- CCC/src/SR2014/SR32014.java
+++ CCC/src/SR2014/SR32014.java
@@ -1,7 +1,29 @@
 package SR2014;
 
+import java.util.Stack;
+
 public class SR32014 {
 
+	public int N = 0;
+	public Stack<Integer> mountain = null;
+	public Stack<Integer> branch = null;
+	
+	public SR32014(int n, int[] original) {
+		this.N = n;
+		this.mountain = new Stack<>();
+		this.branch = new Stack<>();
+		for (int add: original) {
+			mountain.push(add);
+		}
+	}
+	
+	public boolean run() {
+		
+		int current = mountain.pop();
+		
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
