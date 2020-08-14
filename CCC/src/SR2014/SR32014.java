d67cb3cec8b01856c377d819c10b88521d96ab70
selenazhou <selenazhou@selenas-mbp>
2020-08-13 21:00:36 -0400

    passed all test cases

--- CCC/src/SR2014/SR32014.java
+++ CCC/src/SR2014/SR32014.java
@@ -1,5 +1,6 @@
 package SR2014;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class SR32014 {
@@ -19,13 +20,66 @@ public class SR32014 {
 	
 	public boolean run() {
 		
-		int current = mountain.pop();
+		int lowest = 1;
+		int current = 0;
 		
-		return false;
+		while (lowest <= N) {
+			
+			if (!branch.isEmpty()) {
+				current = branch.peek();
+				if (current==lowest) {
+					branch.pop();
+					lowest++;
+					continue;
+				}
+			}
+			
+			if (!mountain.isEmpty()) {
+				current = mountain.pop();
+				if (current==lowest) {
+					lowest++;
+				}
+				else {
+					branch.push(current);
+				}
+			}
+			else {
+				if (!branch.empty()) {
+					return false;
+				}
+			}
+			
+		}
+		
+		return true;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numbRuns = Integer.parseInt(sc.nextLine());
+		
+		for (int run = 0; run < numbRuns; run++) {
+			int total = Integer.parseInt(sc.nextLine());
+			int[] cars = new int[total];
+			
+			for (int i = 0; i < total; i++) {
+				cars[i] = Integer.parseInt(sc.nextLine());
+			}
+			
+			SR32014 game = new SR32014(total, cars);
+			boolean possible = game.run();
+			
+			if (possible) {
+				System.out.println("Y");
+			}
+			else {
+				System.out.println("N");
+			}
+		}
+		
+		sc.close();
 
 	}
 
