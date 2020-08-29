2658a9106d85121ee49a011f6fd3958fd3ce1c5f
Selena <Selena@ACER_SELENA>
2020-08-29 15:18:09 -0400

    tried the array way, still out of memory

--- CCC/src/SR2014/SR42014.java
+++ CCC/src/SR2014/SR42014.java
@@ -1,63 +1,94 @@
 package SR2014;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class SR42014 {
 	
+	public static class Line implements Comparable<Line> {
+		
+		int x = 0;
+		int y1 = 0;
+		int y2 = 0;
+		int tint = 0;
+		
+		public Line(int x, int y1, int y2, int t) {
+			this.x = x;
+			this.y1 = y1;
+			this.y2 = y2;
+			this.tint = t;
+		}
+		
+		public int compareTo(Line arg0) {
+			if (x == arg0.x) {
+				if (tint > 0) {
+					return -1;
+				}
+				else if (arg0.tint > 0){
+					return 1;
+				}
+			}
+			return x - arg0.x;
+		}
+		
+	}
+	
 	int numbGlasses = 0;
 	int threshold = 0;
-	int[][] glasses = null;
-	
-	int maxY;
-	int maxX;
-	int[][] window = null;
+	int maxX = 0;
+	int maxY = 0;
+	ArrayList<Line> glassLines = null;
+	int[] y = null;
 	
 	public SR42014(int N, int t) {
 		this.numbGlasses = N;
 		this.threshold = t;
-		this.glasses = new int[numbGlasses][5];
-		maxY = 0;
-		maxX = 0;
+		this.maxX = 0;
+		this.maxY = 0;
+		this.glassLines = new ArrayList<Line>();
 	}
 	
-	public void addGlass(int index, int x1, int y1, int x2, int y2, int tint) {
-		glasses[index][0] = x1;
-		glasses[index][1] = y1;
-		glasses[index][2] = x2;
-		glasses[index][3] = y2;
-		glasses[index][4] = tint;
-		if (x1 > maxX) {
-			maxX = x1;
-		}
+	public void addGlass(int x1, int y1, int x2, int y2, int tint) {
+		
 		if (x2 > maxX) {
 			maxX = x2;
 		}
-		if (y1 > maxY) {
-			maxY = y1;
+		if (x1 > maxX) {
+			maxX = x1;
 		}
 		if (y2 > maxY) {
 			maxY = y2;
 		}
+		if (y1 > maxY) {
+			maxY = y1;
+		}
+		
+		Line first = new Line(x1, y1, y2-1, tint);
+		this.glassLines.add(first);
+		Line second = new Line(x2, y1, y2-1, tint*(-1));
+		this.glassLines.add(second);
 	}
 	
 	public int run() {
 		
-		this.window = new int[maxY][maxX];
-		
-		for (int index = 0; index < numbGlasses; index++) {
-			int secX = glasses[index][2]-1;
-			int secY = glasses[index][3]-1;
-			for (int firstX = glasses[index][0]; firstX <= secX; firstX++) {
-				for (int firstY = glasses[index][1]; firstY <= secY; firstY++) {
-					window[firstY][firstX] += glasses[index][4];
-				}
-			}
-		}
+		Collections.sort(glassLines);
+		this.y = new int[this.maxY];
 		
 		int area = 0;
-		for (int i = 0; i < maxY; i++) {
-			for (int j = 0; j < maxX; j++) {
-				if (window[i][j] >= this.threshold) {
+		
+		for (int i = 0; i < this.maxX; i++) {
+			for (int l = 0; l < glassLines.size(); l++) {
+				Line line = glassLines.get(l);
+				if (line.x==i) {
+					for (int yCoord = line.y1; yCoord <= line.y2; yCoord++) {
+						y[yCoord] += line.tint;
+					}
+				}
+			}
+			for (int yCoord: y) {
+				if (yCoord >= this.threshold) {
 					area++;
 				}
 			}
@@ -74,7 +105,7 @@ public class SR42014 {
 		SR42014 game = new SR42014(Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine()));
 		
 		for (int i = 0; i < game.numbGlasses; i++) {
-			game.addGlass(i, sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+			game.addGlass(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
 			sc.nextLine();
 		}
 
