4f86f49aaa00a40025fa06c06d138fe1b34a0771
Selena <Selena@10.0.0.107>
2020-09-27 20:04:55 -0400

    done+tested all clear

--- CCC/src/SR2013/SR12013.java
+++ CCC/src/SR2013/SR12013.java
@@ -1,10 +1,43 @@
 package SR2013;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class SR12013 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int input = Integer.parseInt(sc.nextLine());
+		boolean found = false;
+		
+		String currentYear = "";
+		while(!false) {
+			input++;
+			currentYear = Integer.toString(input);
+			char[] currentDigits = currentYear.toCharArray();
+			int[] current = new int[currentDigits.length];
+			for (int i = 0; i < currentDigits.length; i++) {
+				current[i] = currentDigits[i] - '0';
+			}
+			HashMap<Integer, Integer> exists = new HashMap<Integer, Integer>();
+			found = true;
+			for (int i = 0; i < current.length; i++) {
+				if (exists.containsKey(current[i])) {
+					found = false;
+					break;
+				}
+				else {
+					exists.put(current[i], 0);
+				}
+			}
+			if (found) {
+				System.out.println(currentYear);
+				break;
+			}
+		}
+		
 	}
 
 }
