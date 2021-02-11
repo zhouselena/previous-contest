ad8141aceb1dbd13144ee907a6c1841a75501a7f
selenazhou <selenazhou@localhost>
2021-02-10 22:30:18 -0500

    senior 2 2010 all clear

--- CCC/src/SR2010/SR22010.java
+++ CCC/src/SR2010/SR22010.java
@@ -1,10 +1,36 @@
 package SR2010;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class SR22010 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Map<String, Character> alphabet = new HashMap<>();
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < n; i++) {
+			String[] info = sc.nextLine().split(" ");
+			alphabet.put(info[1], info[0].charAt(0));
+		}
+		
+		String code = sc.nextLine();
+		
+		StringBuilder decoded = new StringBuilder();
+		StringBuilder curr = new StringBuilder();
+		for (int i = 0; i < code.length(); i++) {
+			curr.append(code.charAt(i));
+			if (alphabet.containsKey(curr.toString())) {
+				decoded.append(alphabet.get(curr.toString()));
+				curr = new StringBuilder();
+			}
+		}
+		
+		System.out.println(decoded.toString());
+		
 	}
 
 }
