b5b5dfbd9cd65f5c6e559f33b7f41653ca6762e1
selenazhou <selenazhou@localhost>
2021-02-10 22:01:16 -0500

    senior 5 all clear

---
+++ CCC/src/SR2010/SR22010.java
@@ -0,0 +1,10 @@
+package SR2010;
+
+public class SR22010 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CCC/src/SR2011/SR52011.java
+++ CCC/src/SR2011/SR52011.java
@@ -4,7 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class SR52011 {
+	
+	public static class Pair {
+		
+		public int index = 0;
+		public int count = 0;
+		
+		public Pair (int i, int c) {
+			this.index = i;
+			this.count = c;
+		}
+		
+	}
 
 	// for one single cluster
 	public static int numbLightsSwitched(int[] cluster) {
@@ -31,25 +44,57 @@ public class SR52011 {
 			init[i] = Integer.parseInt(sc.nextLine());
 		}
 		
-		ArrayList<int[]> lightsOn = new ArrayList<>();
+		ArrayList<Pair> lightsOn = new ArrayList<>();
 		int count = 0;
 		for (int i = 0; i < N; i++) {
 			if (init[i]==0 && count > 0) {
-				int[] openLightCluster = {i-count, count};
-				lightsOn.add(openLightCluster);
+				lightsOn.add(new Pair(i-count, count));
 				count = 0;
 			}
+			else if (init[i]==1) {
+				count++;
+			}
+		}
+		if (count > 0) {
+			lightsOn.add(new Pair(N-count, count));
 		}
 		
-		int[] numbSwitch = new int[lightsOn.size()];
-		for (int i = numbSwitch.length-1; i > 0; i--) {
-			int[] currentClusterInfo = lightsOn.get(i);
-			int[] currentCluster = new int[currentClusterInfo[1]];
-			for (int t = 0; t < currentClusterInfo[1]; t++) {
-				currentCluster[t] = 1;
+		int[] numbSwitch = new int[lightsOn.size()+1];
+		for (int i = 0; i < numbSwitch.length; i++) {
+			numbSwitch[i] = N;
+		}
+		numbSwitch[lightsOn.size()] = 0;
+		
+		for (int i = lightsOn.size()-1; i >= 0; i--) {
+			int numbLightsOn = 0;
+			for (int j = i; j < lightsOn.size(); j++) {
+				int lightsFromItoJ = lightsOn.get(j).index-lightsOn.get(i).index+lightsOn.get(j).count;
+				if (lightsFromItoJ > 7) {
+					break;
+				}
+				else {
+					numbLightsOn += lightsOn.get(j).count;
+				}
+				
+				if (lightsFromItoJ == 6) {
+					if (init[i+2]==1 && init[i+3]==1) {
+						continue;
+					}
+				}
+				else if (lightsFromItoJ == 7) {
+					if (init[i+3]==1) {
+						continue;
+					}
+				}
+				
+				int turnOff = (lightsFromItoJ<4?4:lightsFromItoJ) - numbLightsOn + numbSwitch[j+1];
+				if (turnOff < numbSwitch[i])
+					numbSwitch[i] = turnOff;
 			}
 			
 		}
+		
+		System.out.println(numbSwitch[0]);
 
 	}
 
